c7707070aac22ddd5ee76ffda1c62dabbdaee22c
Mambue <manuelalejomendez@gmail.com>
2023-07-27 22:17:58 -0300

    Movimiento mas refinado como un señorito

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,20 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
-    private InputManager inputManager;
     private Transform cameraTransform;
+    private InputManager inputManager;
 
+    [SerializeField]
     private bool m_groundedPlayer;
     [SerializeField]
-    private float m_accelerationForce = 20.0f;
+    private float m_accelerationForce = 50.0f;
     [SerializeField]
     private float m_maxPlayerSpeed = 10.0f;
     [SerializeField]
-    private float m_jumpHeight = 3.0f;
+    private float m_jumpForce = 500.0f;
 
     private void Start()
     {
@@ -29,32 +31,32 @@ public class PlayerController : MonoBehaviour
             rb.velocity.Set(rb.velocity.x, 0f, rb.velocity.z);
         }
 
-        if (inputManager.PlayerJumped() && m_groundedPlayer)
+        if (m_groundedPlayer)
         {
-            Jump();
+            MovePlayer();
         }
-        MovePlayer();
     }
 
     void Update()
     {
-        m_groundedPlayer = Mathf.Approximately(rb.velocity.y, 0);
-
+        m_groundedPlayer = rb.velocity.y > -0.01f && rb.velocity.y < 0.01f;
     }
 
     private void MovePlayer()
     {
-        Vector2 playerMovement = inputManager.GetPlayerMovement();
-        Vector3 moveDirection = new Vector3(playerMovement.x, 0f, playerMovement.y);
-        moveDirection = Vector3.Normalize(cameraTransform.forward * moveDirection.z + cameraTransform.right * moveDirection.x);
+        Vector2 playerMoveDir = inputManager.GetPlayerMovement();
+        Vector3 moveDirection = new Vector3(playerMoveDir.x, 0f, playerMoveDir.y);
+        moveDirection = Vector3.Normalize(transform.forward * moveDirection.z + transform.right * moveDirection.x);
+
+        transform.rotation = new Quaternion(transform.rotation.x, cameraTransform.rotation.y, transform.rotation.z, transform.rotation.w);
 
         rb.AddForce(moveDirection * m_accelerationForce * Time.fixedDeltaTime, ForceMode.VelocityChange);
 
         rb.velocity = Vector3.ClampMagnitude(rb.velocity, m_maxPlayerSpeed);
     }
 
-    private void Jump()
+    public void OnJump()
     {
-        rb.AddForce(Vector3.up * m_jumpHeight, ForceMode.Impulse);
+        rb.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse); 
     }
 }
